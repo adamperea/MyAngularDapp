23b7b28a5f699d35249b6f245b28a156214cb6c7
alexytiger <yevseyevich@msn.com>
2019-02-11 23:30:43 -0500

    dig

---
+++ contracts/PokemonAttack.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.5.2;
+
+contract PokemonAttack {
+    
+    //this declares a state variable which means it belongs to the contract's state
+    //This will give us a way to store a string value to the blockchain inside the smart contract.
+    string attack;
+    
+    constructor(string memory initialAttack) public {
+        attack = initialAttack;
+    }
+
+    // event
+    event attackChangedEvent (
+        string _attack
+    );
+	
+    function currentAttack() public view returns(string memory) {
+        return attack;
+    }	
+	
+    function changeAttack(string memory _attack) public { 
+        attack = _attack;
+        emit attackChangedEvent(_attack);
+    }
+
+   
+}
