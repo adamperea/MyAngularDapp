2764e0098c4562ab5e0926c2218c49da25979433
Alex Yevseyevich <alex.yevseyevich@everbank.com>
2019-02-12 19:47:49 -0500

    dig

--- truffle-config.js
+++ truffle-config.js
@@ -44,9 +44,19 @@ module.exports = {
     //
     development: {
       host: "127.0.0.1",     // Localhost (default: none)
-      port: 8545,            // Standard Ethereum port (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
      },
+
+     //Configuration for the Ganache network
+     //the command to deploy to the Ganache will looks like
+     //truffle migrate --network ganache
+     ganache: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // matching any network id
+     },
+
 
     // Another network with more advanced options...
     // advanced: {
@@ -88,10 +98,10 @@ module.exports = {
        version: "0.5.2",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
-       optimizer: {
-          enabled: true,
-          runs: 200
-       },
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
       //  evmVersion: "byzantium"
       //}
     }
