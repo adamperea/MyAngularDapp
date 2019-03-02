029e6686d9d58de33a941d457e02cf717289ab3d
alexytiger <yevseyevich@msn.com>
2019-03-02 13:47:00 -0500

    dig

--- ClientApp/src/app/ethereum/eth.anchor.module.ts
+++ ClientApp/src/app/ethereum/eth.anchor.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 
-// follow the idea of
+// follow the idea to avoid the Circular Dependencies warning
 // https://medium.com/@tomastrajan/total-guide-to-angular-6-dependency-injection-providedin-vs-providers-85b7a347b59f
 
 @NgModule()
